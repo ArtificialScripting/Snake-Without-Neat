9c962dc3d515a40d4706606db23996cd943878e3
ArtificialScripting <53064246+ArtificialScripting@users.noreply.github.com>
2019-07-19 01:24:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,114 +2,12 @@ var scl = 20;
 var speed = 1;
 var snake = [];
 var food = [];
-var agents = 1;
-var nodes = [];
-var weights = [];
 
 function setup() {
   createCanvas(1200, 600).parent("canvas");
-  for (var i=0; i<agents; i++) {
-    snake[i] = new Snake(i);
-    food[i] = new Food();
-  }
+  snake[0] = new Snake(0);
+  food[i] = new Food();
   frameRate(10);
-}
-
-function isOutOfFrame(value, isX) {
-  if (isX) {
-    if (value < 0 || value > width/2) {
-      return true;
-    }
-  }
-  else {
-    if (value < 0 || value > height) {
-      return true;
-    } 
-  }
-  return false;
-}
-
-function getDirection(type) {
-  switch(type) {
-    case 0:
-      return createVector(0, -1);
-      break;
-    case 1:
-      return createVector(1, -1);
-      break;
-    case 2:
-      return createVector(1, 0);
-      break;
-    case 3:
-      return createVector(1, 1);
-      break;
-    case 4:
-      return createVector(0, 1);
-      break;
-    case 5:
-      return createVector(-1, 1);
-      break;
-    case 6:
-      return createVector(-1, 0);
-      break;
-    case 7:
-      return createVector(-1, -1);
-      break;
-  }
-}
-
-function findDistance(type, direction, headpos, id) {
-  switch(type) {
-    case 0:
-      //Body
-      var x = headpos.x;
-      var y = headpos.y;
-      var counter = 0;
-      while (!isOutOfFrame(x, true) && !isOutOfFrame(y, false)) {
-        for (var i=0; i<snake[id].tail.length; i++) {
-          if (x == snake[id].tail[i].x && y == snake[id].tail[i].y) {
-            return counter;
-          }
-        }
-        x+=direction.x*scl;
-        y+=direction.y*scl;
-        counter++;
-      }
-      return 0;
-      break;
-    case 1:
-      //Food
-      var x = headpos.x;
-      var y = headpos.y;
-      var counter = 0;
-      while (!isOutOfFrame(x, true) && !isOutOfFrame(y, false)) {
-        if (x == food[id].x && y == food[id].y) {
-          return counter;
-        }
-
-        x+=direction.x*scl;
-        y+=direction.y*scl;
-        counter++;
-      }
-      return 0;
-      break;
-    case 2:
-      //Wall
-      var x = headpos.x;
-      var y = headpos.y;
-      var counter = 0;
-      while (!isOutOfFrame(x, true) && !isOutOfFrame(y, false)) {
-        x+=direction.x*scl;
-        y+=direction.y*scl;
-        counter++;
-      }
-      return counter;
-      break;
-  }
-}
-
-function sigmoid(t) {
-    return 1/(1+Math.pow(Math.E, -t));
 }
 
 function keyPressed() {
